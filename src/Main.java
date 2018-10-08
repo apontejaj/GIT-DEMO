9c088405c92ad91b5c5cee7de1dcc7027d92edb7
amilcar <apontejaj@gmail.com>
2018-10-08 09:43:11 +0100

    V2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Amilcar sucks");
+		System.out.println("Amilcar sucks");
+		System.out.println("Amilcar sucks");
+		System.out.println("Amilcar sucks");
+		System.out.println("Amilcar sucks");
+		System.out.println("Amilcar sucks");
+		
 	}
 
 }
